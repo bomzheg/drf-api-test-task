20e2dc944285db0df8f0fdef9418f196b1a26908
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-11-13 20:27:07 +0300

    update serializers

--- pools/serialializers.py
+++ pools/serialializers.py
@@ -1,5 +1,8 @@
+from typing import Any, Dict
+
 from rest_framework import serializers
-from .models import Poll, Question
+from .models import Poll, Question, Answer
+
 
 class PoolSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -7,14 +10,34 @@ class PoolSerializer(serializers.Serializer):
     welcome_text = serializers.CharField(max_length=1024)
     final_text = serializers.CharField(max_length=1024)
 
-    def create(self, validated_data):
-        return Poll.objects.create(**validated_data)
+    def create(self, validated_data: Dict[str, Any]):
+        return Poll(**validated_data)
+
+    def update(self, instance: Poll, validated_data: Dict[str, Any]):
+        instance.name = validated_data.get('name', default="name")
+        instance.welcome_text = validated_data.get('welcome_text', default="welcome_text")
+        instance.final_text = validated_data.get('final_text', default="final_text")
+        return instance
 
 
 class QuestionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     question_text = serializers.CharField(max_length=1024)
 
-    def create(self, validated_data):
-        return Question.objects.create(**validated_data)
+    def create(self, validated_data: Dict[str, Any]):
+        return Question(**validated_data)
 
+    def update(self, instance: Question, validated_data: Dict[str, Any]):
+        instance.question_text = validated_data.get('question_text', default="question_text")
+        return instance
+
+
+class AnswerSerializer(serializers.Serializer):
+    answer_text = serializers.CharField(max_length=256)
+
+    def create(self, validated_data: Dict[str, Any]):
+        return Answer(**validated_data)
+
+    def update(self, instance: Answer, validated_data: Dict[str, Any]):
+        instance.answer_text = validated_data.get('answer_text', default="answer_text")
+        return instance

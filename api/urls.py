c4f4e8d58173aa01189342db711486c38dcdb4bb
Yuriy Chebyshev <bomzheg@gmail.com>
2020-11-16 20:53:31 +0300

    add migrations update token authorize

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^api/', include('pools.urls')),
     path(r'admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
--- pools/models.py
+++ pools/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from enum import Enum, auto
 
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class QuestionTypes(Enum):
@@ -62,5 +66,7 @@ class Answer(models.Model):
         return self.answer_text
 
 
-if __name__ == '__main__':
-    print(QuestionTypes.choices())
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)

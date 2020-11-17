41795427b605cd7374f751e3baa2e07aef93477b
Yuriy Chebyshev <bomzheg@gmail.com>
2020-11-17 12:46:30 +0300

    add real answers model and migrate

--- polls/models/plan.py
+++ polls/models/plan.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 from enum import Enum, auto
 
-from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 
 class QuestionTypes(Enum):
@@ -75,11 +71,3 @@ class PossibleAnswer(models.Model):
 
     def __str__(self):
         return self.answer_text
-
-
-# noinspection PyUnusedLocal
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        # noinspection PyUnresolvedReferences
-        Token.objects.create(user=instance)

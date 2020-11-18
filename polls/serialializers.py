34c53dde5d35deb272c21c50914e6b9d2c6ebc95
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-11-18 22:48:51 +0300

    add links between models

--- polls/serialializers.py
+++ polls/serialializers.py
@@ -5,22 +5,30 @@ from polls.models import Poll, Question, PossibleAnswer
 
 
 class PollSerializer(serializers.ModelSerializer):
+    questions = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="question-detail")
+
     class Meta:
         model = Poll
-        exclude = tuple()
-        read_only_fields = ['date_start']
+        fields = ("questions", "name", "date_start", "date_finish",
+                  "description", "welcome_text", "final_text")
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    possible_answers = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="possibleanswer-detail")
+
     class Meta:
         model = Question
-        exclude = tuple()
+        fields = ("possible_answers", "question_text", "question_type", )
+        depth = 1
 
 
 class PossibleAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PossibleAnswer
         exclude = tuple()
+        depth = 1
 
 
 class UserSerializer(serializers.ModelSerializer):
